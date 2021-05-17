d772c14404eeebd683cf58b7d06bd24e22ee7e59
leoxu0257 <enmacorradi@163.com>
2021-05-17 14:04:30 +1200

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+    {
+        title:'What is React?',
+        content: 'React is front end javascript framework',
+    },
+    {
+        title:'Why use React?',
+        content: 'React is favurite',
+    },
+    {
+        title:'How do you use React?',
+        content: 'Use to create components',
+    },
+];
+
+export default ()=>{
+    return (
+        <div>
+            <Accordion items = {items}/>
+        </div>
+    );
+};
