bbc109557ddd1d6764b66c4bf5f5724d67429246
leoxu0257 <enmacorradi@163.com>
2021-05-18 11:12:12 +1200

    add useState

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -8,12 +8,15 @@ const Accordion = ({ items }) => {
     };
  
     const renderedItems = items.map((item,index) => {
+
+        const active = index === activeIndex ? 'active' : '';
+
         return (
         
             <React.Fragment key={item.title}>
 
                 <div 
-                    className="title active"
+                    className= {`title ${active}`}
                     // onClick={()=> console.log('Title clicked',index)}
                     onClick={()=> onTitleClick(index)}
                     
@@ -22,7 +25,7 @@ const Accordion = ({ items }) => {
                     {item.title}
                 </div>
 
-                <div className="content active">
+                <div className= {`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
@@ -32,7 +35,6 @@ const Accordion = ({ items }) => {
     return (
         <div className="ui styled accordion">
             {renderedItems}
-            <h1>{activeIndex}</h1>
         </div>
     );
 };
