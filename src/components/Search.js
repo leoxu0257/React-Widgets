0fe53ac48b9121b89250a1c8b5862e28103decf4
leoxu0257 <enmacorradi@163.com>
2021-05-25 15:17:10 +1200

    may have XSS attacks through the dangerouslySetInnerHTML

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,53 @@
-import React, {useState} from 'react';
-
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search = () => {
-    const [term,setTerm] = useState('');
+    const [term,setTerm] = useState('programing');
+    const [results, setResults] = useState([]);
+
+    useEffect(()=>{
+
+        const search = async () => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params:{
+                    action: 'query',
+                    list : 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                },
+            });
+
+            setResults(data.query.search);
+        };
+        search();
+
+        // declare a helper function and invoke it
+        // (async ()=>{
+        //     await axios.get('123456');
+        // })();
+
+        // Use a Promises 
+        // axios.get('12345')
+        //     .then((response)=>{
+        //         console.log(response.data);
+        //     });
+
+    },[term]);
+
+    const renderedResults = results.map((result)=>{
+        return( 
+            <div key={result.pageid} className="item">
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                    {/* {result.snippet} */}
+                </div>
+            </div>
+        );
+    });
          
     return(
         <div>
@@ -15,6 +60,9 @@ const Search = () => {
                         className="input"
                          />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     );
