f0cae98d81a627d23aa7a556df807d0eacc53cb0
leoxu0257 <enmacorradi@163.com>
2021-05-26 12:26:17 +1200

    fix

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,21 +7,28 @@ const Search = () => {
 
     useEffect(()=>{
 
-        const search = async () => {
-            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
-                params:{
-                    action: 'query',
-                    list : 'search',
-                    origin: '*',
-                    format: 'json',
-                    srsearch: term,
-                },
-            });
+        
 
-            setResults(data.query.search);
-        };
-        search();
+        // const search = async () => {
+        //     const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+        //         params:{
+        //             action: 'query',
+        //             list : 'search',
+        //             origin: '*',
+        //             format: 'json',
+        //             srsearch: term,
+        //         },
+        //     });
 
+        //     setResults(data.query.search);
+        // };
+
+        // const timeoutId = setTimeout(()=>{
+        //     if (term) {
+        //         search();
+        //     }
+        // },500);
+        
         // declare a helper function and invoke it
         // (async ()=>{
         //     await axios.get('123456');
@@ -38,6 +45,13 @@ const Search = () => {
     const renderedResults = results.map((result)=>{
         return( 
             <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a 
+                        className=" ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >Go
+                    </a>
+                </div>
                 <div className="content">
                     <div className="header">
                         {result.title}
