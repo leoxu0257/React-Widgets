f6e65dd01d138ea8045dd9b5dcf2f152ea68faac
leoxu0257 <enmacorradi@163.com>
2021-06-08 16:08:37 +1200

    add

--- src/App.js
+++ src/App.js
@@ -35,30 +35,13 @@ const options = [
     }
 ];
 
-const showAccordion = () => {
-    if (window.location.pathname === '/') {
-        return <Accordion  items={items}/>;
-    }
-}
-
-const showList = () => {
-    if (window.location.pathname === '/list') {
-        return <Search  />;
-    }
-}
-
-const showDropdown = () => {
-    if (window.location.pathname === '/dropdown') {
-        return <Dropdown />;
-    }
-}
  
 
 export default ()=>{
     const [selected, setSelected] = useState(options[0]);
     return (
         <div> 
-            <Header>
+            <Header />
             <Route path="/">
                 <Accordion items={items} />
             </Route>
